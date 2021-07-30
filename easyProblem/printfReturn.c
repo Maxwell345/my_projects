01e557d0a87d03354e73f03431cdb8d50f5a1d40
Maxwell345 <82862761+Maxwell345@users.noreply.github.com>
2021-07-30 10:19:05 +0800

    Add files via upload

---
+++ easyProblem/printfReturn.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	printf("%d\n",printf("Hello,World!\n"));
+	char word[20];
+	scanf("%s",word);
+	printf("%d\n",printf("%s\n",word));
+	int a,b;
+	scanf("%d",&a);
+	b=printf("%d\n",a);
+	printf("%d",b);
+}
