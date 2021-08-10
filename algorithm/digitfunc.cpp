8b7aad7276cacea0cf4bb569a38817fd6f101b2d
Maxwell345 <82862761+Maxwell345@users.noreply.github.com>
2021-08-10 11:18:41 +0800

    Add files via upload

---
+++ algorithm/digitfunc.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int tenpow(int y)
+{
+    int ans = 1;
+    while (y--) {
+        ans *= 10;
+    }
+    return ans;
+}
+int dgt(int d, int order)
+{
+    return (d % tenpow(order)) / tenpow(order - 1);
+}
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+    cout << dgt(n, k);
+}
