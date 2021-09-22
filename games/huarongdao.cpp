a2524592d370aad87aff254370d12bdd9b321888
Maxwell345 <82862761+Maxwell345@users.noreply.github.com>
2021-09-22 22:12:22 +0800

    Add files via upload

---
+++ games/huarongdao.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    int i, j,flag;
+    char up, down, left, right;
+    char key;
+    char block[6][6] = { "#####",
+                         "#123#",
+                         "#456#",
+                         "#78 #",
+                         "#####"};
+    cout << "\n\n\n\n\n\n       欢迎来到华容道游戏\n";
+    cout << "       请按相应数字来移动\n\n";
+    cout << "       单人游戏，请自行打乱\n\n\n";
+    cout << "       按任意键开始游戏";
+    _getch();
+    system("cls");
+    cout<<"\n\n\n\n\n\n";
+    for (i = 1; i < 4; i++){
+    	cout<<"       ";
+        for (j = 1; j < 4; j++)
+            cout<< "  " << block[i][j]<<"  ";
+        cout<<endl;
+    }  
+    while (1) {
+        key = _getch();
+        for(i=1;i<4;i++)
+            for(j=1;j<4;j++)
+                if (block[i][j] == key){
+                    if (block[i - 1][j] == ' '){
+                        block[i - 1][j] = key;
+                        block[i][j] = 32;
+                    }
+                    else if (block[i + 1][j] == ' ') {
+                        block[i + 1][j] = key;
+                        block[i][j] = 32;
+                    }
+                    else if (block[i][j - 1] == ' ') {
+                        block[i][j - 1] = key;
+                        block[i][j] = 32;
+                    }
+                    else if (block[i][j + 1] == ' ') {
+                        block[i][j + 1] = key;
+                        block[i][j] = 32;
+                    }
+                    goto pace;
+                }
+    pace:
+        system("cls");
+        cout<<"\n\n\n\n\n\n";
+        for (i = 1; i < 4; i++){
+        	cout<<"       ";
+        	for (j = 1; j < 4; j++)
+            	cout<< "  " << block[i][j]<<"  ";
+        	cout<<endl;
+    	}
+        flag = 1;
+        for (i = 1; i < 4; i++)
+            for (j = 1; j < 4; j++)
+                if (block[i][j] != 3 * (i - 1) + j+48&&block[i][j]!=32) {
+                    flag = 0; goto chaos;
+                }  
+    chaos:
+        if (flag) {
+            cout << "恭喜您，游戏胜利！\n";
+            break;
+        }
+    }
+    Sleep(2500);
+}
