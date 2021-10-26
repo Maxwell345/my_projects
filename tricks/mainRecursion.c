7081639dd6e0e2f4bf5d1bb0924ad7a28d2e5ddb
Maxwell345 <82862761+Maxwell345@users.noreply.github.com>
2021-10-26 21:45:50 +0800

    Delete tricks directory

--- tricks/mainRecursion.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int a;
-int main()
-{
-	if(a>=100) return 0;
-	printf(a%10==0?"%d\n":"%d ",++a);
-	main();
-}
--- tricks/statedef.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#define f(x) x;x;x;x;x;x;x;x;x;x;
-int n;
-int main()
-{
-	f( f(printf("%d ",++n))  printf("\n"); )
-	return 0;
-}
